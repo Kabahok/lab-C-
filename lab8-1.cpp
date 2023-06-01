6809e8b87aa3f9bd3114406e0e6275d383eab87c
egor avdeev <82036345+Kabahok@users.noreply.github.com>
2023-06-01 09:18:24 +0300

    Create lab8-1.cpp

---
+++ lab8-1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+string f(string input) {
+   string output;
+   string item;
+   istringstream iss(input);
+
+   while (iss >> item) {
+       if (item == "no") {
+           output += ",";
+       }
+       output += item + " ";
+   }
+   return output;
+}
+
+
+int main() {
+
+//    string input;
+//    getline(cin, input)
+
+   string input = "This is no a test, no just a sample string";
+   cout << "Введенная строка: " + input << endl << endl;
+   string output = f(input);
+   cout << "Итоговая строка: " << output << std::endl;
+
+   return 0;
+}
