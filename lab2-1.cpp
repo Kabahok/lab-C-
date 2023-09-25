b6a14ef29fc20408c9e18de41487afb94fc74aa6
egor avdeev <82036345+Kabahok@users.noreply.github.com>
2023-09-25 11:25:37 +0300

    Update lab2-1.cpp

--- lab2-1.cpp
+++ lab2-1.cpp
@@ -1,23 +1,115 @@
 #include <iostream>
-#include <math.h>
 using namespace std;
 
-int main() {
-    double x, y;
+struct Node {
+    int data;
+    Node* next;
+};
 
-    cout << "Введите значение х:" << endl;
-    cin >> x;
-
-    if (x > 2.5) {
-        y =  (x + 1);
-    } else if (x >= 0 && x < 2) {
-        y =  (1 + pow(x,5));
-    } else {
-        y =  (x + log(abs(sin(x))));
+class Stack {
+public:
+    Stack() {
+        top = nullptr;
     }
+    
+    void push(int value) {
+        Node* newNode = new Node;
+        newNode->data = value;
+        newNode-> next = top;
+        top = newNode;
+    }
+    
+    int pop() {
+        if (isEmpty()) {
+            std::cout << "Стек пуст!" << std::endl;
+            return -1;
+        }
+        
+        int value = top->data;
+        Node* temp = top;
+        top = top->next;
+        delete temp;
+        return value;
+    }
+    
+    int peek() {
+        if (isEmpty()) {
+            std::cout << "Стек пуст!" << std::endl;
+            return -1;
+        }
+        return top->data;
+    }
+    
+    
+    bool isEmpty() {
+        return top == nullptr;
+    }
+    
+private:
+    Node* top;
+};
 
-    cout << "Значение y равно: " << y << endl;
+void swapMinMax(Stack& stack) {
+    if (stack.isEmpty()) {
+        std::cout << "Стек пуст!" << std::endl;
+    }
+    
+    int minValue = stack.peek();
+    int maxValue = stack.peek();
+    
+    Stack tempStack;
+    
+    while (!stack.isEmpty()) {
+        int currentValue = stack.pop();
+        
+        if (currentValue < minValue) {
+            minValue = currentValue;
+        } else if (currentValue > maxValue) {
+            maxValue = currentValue;
+        }
+        
+        tempStack.push(currentValue);
+    }
+    
+    while(!tempStack.isEmpty()) {
+        int currentValue = tempStack.pop();
+        
+        if (currentValue == minValue) {
+            stack.push(maxValue);
+        } else if (currentValue == maxValue) {
+            stack.push(minValue);
+        } else {
+            stack.push(currentValue);
+        }
+    }
+};
 
-    cout << y << endl;
-    return 0;
+void fillStack(Stack& stack) {
+    stack.push(5);
+    stack.push(3);
+    stack.push(1);
+    stack.push(4);
+    stack.push(2);
+}
+
+void outputStack(Stack& myStack) {
+    while (!myStack.isEmpty()) {
+        std::cout << myStack.pop() << " ";
+    }
+};
+
+int main() {
+    Stack myStack;
+    
+    fillStack(myStack);
+    
+    std::cout << "Изначальный стек: ";
+    outputStack(myStack);
+    std::cout << "" << std::endl;
+    
+    fillStack(myStack);
+    swapMinMax(myStack);
+    
+    std::cout << "Стек после перетановки: ";
+    outputStack(myStack);
 }
